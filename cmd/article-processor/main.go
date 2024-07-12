1a9d762e20ab6eeb3344c3944044483f2d804183
Nicolas Garza <nicolas.garza@gmail.com>
2024-07-11 22:41:14 -0500

    worker pool implemented for ingesting from rabbitmq

--- cmd/article-processor/main.go
+++ cmd/article-processor/main.go
@@ -7,6 +7,7 @@ import (
 	"news-crawler/internal/queue"
 	"news-crawler/internal/storage"
 	"news-crawler/internal/utils"
+	"news-crawler/internal/workers"
 	"time"
 )
 
@@ -25,6 +26,10 @@ func main() {
 		log.Fatalf("Failed to connect to Mongodb: %v", err)
 	}
 
+	workerPool := workers.NewWorkerPool(5)
+	workerPool.Start()
+	defer workerPool.Stop()
+
 	msgs, err := rabbitMQ.Consume()
 	if err != nil {
 		log.Fatalf("Failed to register a consumer: %v", err)
@@ -34,48 +39,49 @@ func main() {
 
 	for msg := range msgs {
 		articleUrl := string(msg.Body)
-		log.Printf("Received a message: %s", articleUrl)
-
-		articleProcessor := crawler.NewScraper(articleUrl)
-		articleInfo, err := articleProcessor.ScrapeArticle()
-		if err != nil {
-			log.Printf("Failed to scrape article %s: %v", articleUrl, err)
-			log.Println("-----------------------------------")
-			continue
-		}
-
-		article := &models.Article{
-			ID:      utils.GenerateID(),
-			Title:   articleInfo.Title,
-			Authors: articleInfo.Authors,
-			Date:    articleInfo.Date,
-			Content: articleInfo.Content,
-			URL:     articleUrl,
-		}
-
-		err = esClient.IndexArticle(article)
-		if err != nil {
-			log.Printf("Failed to index article in Elasticsearch: %v", err)
-		}
-
-		metadata := &models.Metadata{
-			ArticleID: article.ID,
-			URL:       articleUrl,
-			ScrapedAt: time.Now(),
-			Source:    "Reuters",
-		}
-
-		err = mongoClient.InsertMetadata(metadata)
-		if err != nil {
-			log.Printf("Failed to insert metadata in MongoDB: %v", err)
-		}
-
-		log.Printf("Article title: %s", articleInfo.Title)
-		log.Printf("Article authors: %q", &articleInfo.Authors)
-		log.Printf("Article date: %v", &articleInfo.Date)
-		log.Printf("Article content length: %d characters", len(articleInfo.Content))
-		log.Println("-----------------------------------")
-		time.Sleep(time.Second * 3)
-
+		workerPool.Submit(func() {
+			processArticle(articleUrl, esClient, mongoClient)
+		})
 	}
+}
+
+func processArticle(articleUrl string, esClient *storage.ElasticSearchClient, mongoClient *storage.MongoDBClient) {
+	log.Printf("Received a message: %s", articleUrl)
+
+	articleProcessor := crawler.NewScraper(articleUrl)
+	articleInfo, err := articleProcessor.ScrapeArticle()
+	if err != nil {
+		log.Printf("Failed to scrape article %s: %v", articleUrl, err)
+		log.Println("-----------------------------------")
+		return
+	}
+
+	article := &models.Article{
+		ID:      utils.GenerateID(),
+		Title:   articleInfo.Title,
+		Authors: articleInfo.Authors,
+		Date:    articleInfo.Date,
+		Content: articleInfo.Content,
+		URL:     articleUrl,
+	}
+
+	err = esClient.IndexArticle(article)
+	if err != nil {
+		log.Printf("Failed to index article in Elasticsearch: %v", err)
+	}
+
+	metadata := &models.Metadata{
+		ArticleID: article.ID,
+		URL:       articleUrl,
+		ScrapedAt: time.Now(),
+		Source:    "Reuters",
+	}
+
+	err = mongoClient.InsertMetadata(metadata)
+	if err != nil {
+		log.Printf("Failed to insert metadata in MongoDB: %v", err)
+	}
+
+	log.Println("Processed article: %s", article.Title)
+	time.Sleep(time.Second * 1)
 }
